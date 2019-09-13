4b9e0f7aa96956a923e0f2c8502d0af5c2858aa2
datsuns <the.skeleton7@gmail.com>
2019-09-13 13:52:50 +0900

    add: custom window wrapped ListBox

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 	//"github.com/lxn/win"
@@ -11,63 +11,66 @@ import (
 
 func main() {
 	var mw *walk.MainWindow
-	var inTE, outTE *walk.TextEdit
-	var lb *walk.ListBox
-	var model *EnvModel = NewEnvModel()
-	itemCountChangedPublisher := walk.EventPublisher{}
+	//var inTE, outTE *walk.TextEdit
+	//var lb *walk.ListBox
+	//var model *EnvModel = NewEnvModel()
+	//itemCountChangedPublisher := walk.EventPublisher{}
 
 	MainWindow{
 		AssignTo: &mw,
 		Title:    "SCREAMO",
 		MinSize:  Size{600, 400},
-		Layout:   VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					TextEdit{
-						AssignTo: &inTE,
-						OnKeyDown: func(key walk.Key) {
-							if key == walk.KeyReturn {
-								fmt.Println("Return")
-							}
-						},
-					},
-					TextEdit{
-						AssignTo: &outTE,
-						ReadOnly: true,
-					},
-					ListBox{
-						AssignTo: &lb,
-						Model:    model,
-						//OnCurrentIndexChanged: mw.lb_CurrentIndexChanged,
-						//OnItemActivated:       mw.lb_ItemActivated,
-						OnKeyDown: func(key walk.Key) {
-							if key == walk.KeyRight {
-								fmt.Println("index:%v", lb.CurrentIndex())
-							}
-							if key == walk.KeyF1 {
-								fmt.Println("down")
-								now := lb.CurrentIndex()
-								lb.SetCurrentIndex(now - 1)
-								fmt.Printf("now[%v] set to [%v]\n", now, lb.CurrentIndex())
-								lb.CurrentIndexChanged()
-								//lb.SendMessage(win.LB_SETCURSEL, uintptr(lb.CurrentIndex()), 0)
-							}
-							if key == walk.KeyK {
-								fmt.Println("K")
-							}
-						},
-					},
-				},
-			},
-		},
+		Layout:   HBox{},
+		//Children: []Widget{
+		//	HSplitter{
+		//		Children: []Widget{
+		//			TextEdit{
+		//				AssignTo: &inTE,
+		//				OnKeyDown: func(key walk.Key) {
+		//					if key == walk.KeyReturn {
+		//						fmt.Println("Return")
+		//					}
+		//				},
+		//			},
+		//			TextEdit{
+		//				AssignTo: &outTE,
+		//				ReadOnly: true,
+		//			},
+		//			//ListBox{
+		//			//	AssignTo: &lb,
+		//			//	Model:    model,
+		//			//	//OnCurrentIndexChanged: mw.lb_CurrentIndexChanged,
+		//			//	//OnItemActivated:       mw.lb_ItemActivated,
+		//			//	OnKeyDown: func(key walk.Key) {
+		//			//		if key == walk.KeyRight {
+		//			//			fmt.Println("index:%v", lb.CurrentIndex())
+		//			//		}
+		//			//		if key == walk.KeyF1 {
+		//			//			fmt.Println("down")
+		//			//			now := lb.CurrentIndex()
+		//			//			lb.SetCurrentIndex(now - 1)
+		//			//			fmt.Printf("now[%v] set to [%v]\n", now, lb.CurrentIndex())
+		//			//			lb.CurrentIndexChanged()
+		//			//			//lb.SendMessage(win.LB_SETCURSEL, uintptr(lb.CurrentIndex()), 0)
+		//			//		}
+		//			//		if key == walk.KeyK {
+		//			//			fmt.Println("K")
+		//			//		}
+		//			//	},
+		//			//},
+		//		},
+		//	},
 	}.Create()
-	mw.MustRegisterProperty("ItemCount", walk.NewReadOnlyProperty(
-		func() interface{} {
-			return model.ItemCount()
-		},
-		itemCountChangedPublisher.Event(),
-	))
+
+	NewMyListBox(mw)
+	NewMyListBox(mw)
+
+	//mw.MustRegisterProperty("ItemCount", walk.NewReadOnlyProperty(
+	//	func() interface{} {
+	//		return model.ItemCount()
+	//	},
+	//	itemCountChangedPublisher.Event(),
+	//))
 	mw.Run()
 }
 
