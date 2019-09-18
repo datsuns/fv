2d37ec3e951aba40266e433cf7d6984906bbc403
datsuns <the.skeleton7@gmail.com>
2019-09-18 16:01:20 +0900

    add: sample column

--- view.go
+++ view.go
@@ -8,12 +8,12 @@ import (
 	"github.com/lxn/win"
 )
 
-type MyListBox struct {
-	*walk.ListBox
+type MyView struct {
+	*walk.TableView
 	model   *MyModel
 	name    string
 	key     walk.Key
-	neigbor *MyListBox
+	neigbor *MyView
 }
 
 var (
@@ -23,48 +23,52 @@ var (
 	}
 )
 
-func NewMyListBox(parent walk.Container, name string, root string) (*MyListBox, error) {
-	lb, err := walk.NewListBox(parent)
+func NewMyView(parent walk.Container, name string, root string) (*MyView, error) {
+	t, err := walk.NewTableView(parent)
 	if err != nil {
 		return nil, err
 	}
-	mlb := &MyListBox{lb, NewMyModel(root), name, 0, nil}
-	lb.SetModel(mlb.model)
+	col := walk.NewTableViewColumn()
+	col.SetName("path")
+	t.Columns().Clear()
+	t.Columns().Add(col)
+	mv := &MyView{t, NewMyModel(root), name, 0, nil}
+	t.SetModel(mv.model)
 
-	if err := walk.InitWrapperWindow(mlb); err != nil {
+	if err := walk.InitWrapperWindow(mv); err != nil {
 		return nil, err
 	}
 
-	return mlb, nil
+	return mv, nil
 }
 
-func (mlb *MyListBox) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
-	//fmt.Printf("[%v] [%v]\n", mlb.name, Msg2Str(msg))
-	switch msg {
-	case win.WM_KEYDOWN:
-		if walk.Key(wParam) == mlb.key {
-			fmt.Printf("[%s] change forcus\n", mlb.name)
-			mlb.neigbor.SetFocus()
-			return 0
-		} else if walk.Key(wParam) == walk.KeyReturn {
-			idx := mlb.ListBox.CurrentIndex()
-			v, _ := mlb.model.Value(idx).(string)
-			fmt.Printf("[%s] return w/ [%v:%v]\n", mlb.name, idx, v)
-			mlb.ListBox.WndProc(hwnd, msg, wParam, lParam)
-			mlb.model.Enter(v)
-			mlb.ListBox.SetModel(mlb.model)
-		} else if mapped, exists := keyMap[walk.Key(wParam)]; exists {
-			return mlb.ListBox.WidgetBase.WndProc(hwnd, msg, uintptr(mapped), lParam)
-		}
-	case win.WM_CHAR:
-		return 0
-	}
-	return mlb.ListBox.WndProc(hwnd, msg, wParam, lParam)
+func (mv *MyView) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	fmt.Printf("[%v] [%v]\n", mv.name, Msg2Str(msg))
+	//switch msg {
+	//case win.WM_KEYDOWN:
+	//	if walk.Key(wParam) == mv.key {
+	//		fmt.Printf("[%s] change forcus\n", mv.name)
+	//		mv.neigbor.SetFocus()
+	//		return 0
+	//	} else if walk.Key(wParam) == walk.KeyReturn {
+	//		idx := mv.ListBox.CurrentIndex()
+	//		v, _ := mv.model.Value(idx).(string)
+	//		fmt.Printf("[%s] return w/ [%v:%v]\n", mv.name, idx, v)
+	//		mv.ListBox.WndProc(hwnd, msg, wParam, lParam)
+	//		mv.model.Enter(v)
+	//		mv.ListBox.SetModel(mv.model)
+	//	} else if mapped, exists := keyMap[walk.Key(wParam)]; exists {
+	//		return mv.ListBox.WidgetBase.WndProc(hwnd, msg, uintptr(mapped), lParam)
+	//	}
+	//case win.WM_CHAR:
+	//	return 0
+	//}
+	return mv.TableView.WndProc(hwnd, msg, wParam, lParam)
 }
 
-func (mlb *MyListBox) RegisterNeigbor(neigbor *MyListBox, key walk.Key) {
-	mlb.key = key
-	mlb.neigbor = neigbor
+func (mv *MyView) RegisterNeigbor(neigbor *MyView, key walk.Key) {
+	mv.key = key
+	mv.neigbor = neigbor
 }
 
 type MyModel struct {
