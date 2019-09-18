4492c11f443cb92345ec2141ce55c09ed5c0efb5
datsuns <the.skeleton7@gmail.com>
2019-09-18 14:36:20 +0900

    add: specify font

--- main.go
+++ main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/walk/declarative"
 	"github.com/lxn/win"
 )
+
+var f declarative.Font = declarative.Font{
+	PointSize: 20,
+}
 
 func main() {
 	var mw *walk.MainWindow
 
 	fmt.Println("hello")
 
-	MainWindow{
+	declarative.MainWindow{
 		AssignTo: &mw,
 		Title:    "SCREAMO",
-		MinSize:  Size{600, 400},
-		Layout:   HBox{},
+		MinSize:  declarative.Size{600, 400},
+		Layout:   declarative.HBox{},
+		Font:     f,
 	}.Create()
 
 	left, err := NewMyListBox(mw, "LEFT", ".")
