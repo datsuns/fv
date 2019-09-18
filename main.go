c160fa377bb0cb2132de1c7ee849f97584224324
datsuns <the.skeleton7@gmail.com>
2019-09-18 13:07:23 +0900

    del: unnecessary comment

--- main.go
+++ main.go
@@ -17,45 +17,6 @@ func main() {
 		Title:    "SCREAMO",
 		MinSize:  Size{600, 400},
 		Layout:   HBox{},
-		//Children: []Widget{
-		//	HSplitter{
-		//		Children: []Widget{
-		//			TextEdit{
-		//				AssignTo: &inTE,
-		//				OnKeyDown: func(key walk.Key) {
-		//					if key == walk.KeyReturn {
-		//						fmt.Println("Return")
-		//					}
-		//				},
-		//			},
-		//			TextEdit{
-		//				AssignTo: &outTE,
-		//				ReadOnly: true,
-		//			},
-		//			//ListBox{
-		//			//	AssignTo: &lb,
-		//			//	Model:    model,
-		//			//	//OnCurrentIndexChanged: mw.lb_CurrentIndexChanged,
-		//			//	//OnItemActivated:       mw.lb_ItemActivated,
-		//			//	OnKeyDown: func(key walk.Key) {
-		//			//		if key == walk.KeyRight {
-		//			//			fmt.Println("index:%v", lb.CurrentIndex())
-		//			//		}
-		//			//		if key == walk.KeyF1 {
-		//			//			fmt.Println("down")
-		//			//			now := lb.CurrentIndex()
-		//			//			lb.SetCurrentIndex(now - 1)
-		//			//			fmt.Printf("now[%v] set to [%v]\n", now, lb.CurrentIndex())
-		//			//			lb.CurrentIndexChanged()
-		//			//			//lb.SendMessage(win.LB_SETCURSEL, uintptr(lb.CurrentIndex()), 0)
-		//			//		}
-		//			//		if key == walk.KeyK {
-		//			//			fmt.Println("K")
-		//			//		}
-		//			//	},
-		//			//},
-		//		},
-		//	},
 	}.Create()
 
 	left, err := NewMyListBox(mw, "LEFT", "/")
@@ -68,13 +29,6 @@ func main() {
 	}
 	left.RegisterNeigbor(right, walk.KeyL)
 	right.RegisterNeigbor(left, walk.KeyH)
-
-	//mw.MustRegisterProperty("ItemCount", walk.NewReadOnlyProperty(
-	//	func() interface{} {
-	//		return model.ItemCount()
-	//	},
-	//	itemCountChangedPublisher.Event(),
-	//))
 	mw.Run()
 }
 
